ea33f3133ca5bce66f84c93a73e1d9c107a9cbff
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2022-02-10 14:11:25 +0700

    Date 09 02 2022

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
+    date_09_02_2022();
+}
+
+fn date_09_02_2022 () {
+    let arr = [1,2,3,4,5];
+    let mut result = [1; 5];
+    for item in arr.iter().enumerate() {
+        let (index, _): (usize, _) = item;
+        for item1 in arr.iter().enumerate() {
+            let (index1, elm1): (usize, &i32) = item1;
+            if index != index1 {
+                result[index] *= elm1;
+            }
+        }
+    }
+
+    println!("{:?}", result)
 }
