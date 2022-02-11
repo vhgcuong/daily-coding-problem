7b00acc3d85409985faac29f829d8aacafe022d0
Cuong.VuHung <cuong.vuhung@vti.com.vn>
2022-02-11 14:39:57 +0700

    08 02 2022

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("Hello, world!");
     date_09_02_2022();
+    date_08_02_2022();
 }
 
 fn date_09_02_2022 () {
@@ -17,4 +18,27 @@ fn date_09_02_2022 () {
     }
 
     println!("{:?}", result)
+}
+
+fn date_08_02_2022 () {
+    let arr = [1, 4, 6, 9, 10];
+    let k = 10;
+    let mut result = Vec::new();
+
+    for item in arr.iter().enumerate() {
+        let (inx1, val1) = item;
+        for item2 in arr.iter().enumerate() {
+            let (inx2, val2) = item2;
+            if val1 + val2 == k && inx1 != inx2 {
+                if !result.contains(&inx1) && !result.contains(&inx2) {
+                    result.push(inx1);
+                    result.push(inx2);
+
+                    println!("{} + {} = {}", arr[inx1], arr[inx2], k);
+                }
+            }
+        }
+    }
+
+    println!("{:?}", result);
 }
