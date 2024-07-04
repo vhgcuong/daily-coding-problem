0b6772208a3766c2b1d56a47f8742415e4d7999f
Cuong D9 <vhgcuong95@gmail.com>
2024-07-04 17:31:07 +0700

    832

--- python3/832/main.py
+++ python3/832/main.py
@@ -8,4 +8,17 @@ For example, given the array [5, 1, 3, 5, 2, 3, 4, 1], return 5 as the longest s
 
 
 if __name__ == "__main__":
-    pass
+    arr = [5, 1, 3, 5, 2, 3, 4, 1]
+    subs = {}
+    item = []
+    for index, value in enumerate(arr):
+        if value in item and len(item) > 0:
+            subs[len(item)] = item
+            item = []  
+        item.append(value)
+
+    if len(item) > 0: 
+        subs[len(item)] = item
+
+    max_key = max(subs.keys())
+    print(subs[max_key])
